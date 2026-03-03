4afa966c273665c77aea997d5a647d1d0de2e81a
mayowasamuel <fpasamuelmayowa51@gmail.com>
2026-03-03 21:34:41 +0100

    ne form

--- firebase.jsx
+++ firebase.jsx
@@ -6,13 +6,21 @@ import { getMessaging } from "firebase/messaging";
 
 // Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_KEY,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
+  // apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  // authDomain: import.meta.env.VITE_authDomain,
+  // projectId: import.meta.env.VITE_projectId,
+  // storageBucket: import.meta.env.VITE_storageBucket,
+  // messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  // appId: import.meta.env.VITE_appId,
+  // measurementId: import.meta.env.VITE_measurementId,
+
+   apiKey: "AIzaSyDjBnmik_7FCYzSJYktYeuKHqNtxLRebfs",
+  authDomain: "mypromopsherereborn.firebaseapp.com",
+  projectId: "mypromopsherereborn",
+  storageBucket: "mypromopsherereborn.firebasestorage.app",
+  messagingSenderId: "1088107194704",
+  appId: "1:1088107194704:web:eeabd28fd0049838e4e455",
+  measurementId: "G-RY12QKXE3H"
 };
 
 
